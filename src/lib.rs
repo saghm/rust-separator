f8289a75c519e791d4f228404cabb53de007b9aa
Saghm Rossi <saghmrossi@gmail.com>
2015-07-30 11:02:41 -0400

    added initial documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 pub mod unsigned_int;
 pub mod signed_int;
 
+/// Used for numbers that can be printed with separators for the thousands places.
 pub trait Separatable {
+
+    /// Converts the number to a string with thousands separator.
     fn separated_string(&self) -> String;
 }
